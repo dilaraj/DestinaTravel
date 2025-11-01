09a864291dc6a359d3a3185913b97f8a2a232fec
Dilara Ajaj <106478364+dilaraj@users.noreply.github.com>
2025-11-01 14:04:01 +0000

    Completing footer component

---
+++ src/components/FooterComponent/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <div className="footer-container">
+            <div className="footer-title">Destina.®</div>
+            <div className="footer-date">EST. 2025</div>
+        </div>
+    )
+}
