da3ec856e1802b3e73c104fe833eea34a309b23d
Dilara Ajaj <106478364+dilaraj@users.noreply.github.com>
2025-11-01 13:23:14 +0000

    Finalising DestinationCard component

--- src/components/DestinationCardComponent/DestinationCard.jsx
+++ src/components/DestinationCardComponent/DestinationCard.jsx
@@ -1,21 +1,37 @@
 import './DestinationCard.css';
 import FrontCard from "./FrontCardComponent/FrontCard";
 import { destinations } from "../../data/destination";
+import BackCard from './BackCardComponent/BackCard';
 
 export default function DestinationCard() {
     return (
         <>
             <div className="destinationCard-container">
                 {destinations.map(dest => 
-                    <FrontCard
-                        image={dest.image}
-                        city={dest.name}
-                        country={dest.country}
-                        cost={dest.cost}
-                        type={dest.type}
-                    />
+                    <>
+                    <div className="destinationCard-front">
+                        <FrontCard
+                            image={dest.image}
+                            city={dest.name}
+                            country={dest.country}
+                            cost={dest.cost}
+                            type={dest.type}
+                        />
+                    </div>
+                    <div className="destinationCard-back">
+                        <BackCard
+                            image={dest.image}
+                            city={dest.name}
+                            country={dest.country}
+                            price={dest.cost}
+                            description={dest.description}
+                            type={dest.type}
+                        />
+                    </div>
+                    </>
                 )}
             </div>
+            
         </>
     )
 }
